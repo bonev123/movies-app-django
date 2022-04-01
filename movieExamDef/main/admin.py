c47375c47eb9a428e3e96b6de37d29f17c825d39
bonev1234 <bonev123_1996@abv.bg>
2022-04-01 17:12:21 +0300

    added admin functionality

--- movieExamDef/main/admin.py
+++ movieExamDef/main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from movieExamDef.main.models import Movie, MoviePhoto
+
+
+class MovieInlineAdmin(admin.StackedInline):
+    model = Movie
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('movie_name', 'genre')
+
+
+@admin.register(MoviePhoto)
+class MoviePhotoAdmin(admin.ModelAdmin):
+    pass
+
