58e8aa5d30d348dd1d73a44f059257b6af9a7aed
bonev1234 <bonev123_1996@abv.bg>
2022-04-10 13:22:27 +0300

    added new  home, login and register templates

--- movieExamDef/accounts/views.py
+++ movieExamDef/accounts/views.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.views import LoginView, PasswordChangeView
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView
-from movieExamDef.accounts.forms import CreateProfileForm, DeleteProfileForm
+from movieExamDef.accounts.forms import CreateProfileForm
 from movieExamDef.accounts.models import Profile
 from movieExamDef.main.models import Movie, MoviePhoto
 
 
 class UserLoginView(LoginView):
-    template_name = 'accounts/login_page.html'
+    template_name = 'accounts/login_page-test.html'
     success_url = reverse_lazy('dashboard')
 
     def get_success_url(self):
@@ -22,18 +23,17 @@ class ChangeUserPasswordView(PasswordChangeView):
 
 class UserRegisterView(CreateView):
     form_class = CreateProfileForm
-    template_name = 'accounts/profile_create.html'
+    template_name = 'accounts/profile_create-test.html'
     success_url = reverse_lazy('dashboard')
 
 
 class ProfileDetailsView(DetailView):
     model = Profile
-    template_name = 'accounts/profile_details.html'
+    template_name = 'accounts/profile_details-test.html'
     context_object_name = 'profile'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # self.object is a Profile instance
         movies = list(Movie.objects.filter(user_id=self.object.user_id))
 
         movie_photos = MoviePhoto.objects \
@@ -49,31 +49,6 @@ class ProfileDetailsView(DetailView):
             'movies': movies,
         })
         return context
-
-
-# def get_profile():
-#     profiles = Profile.objects.all()
-#     if profiles:
-#         return profiles[0]
-#     return None
-#
-#
-
-# def delete_profile(request):
-#     profile = get_profile()
-#
-#     if request.method == 'POST':
-#         form = DeleteProfileForm(request.POST, request.FILES, instance=profile)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('index')
-#     else:
-#         form = DeleteProfileForm(instance=profile)
-#
-#     context = {
-#         'form': form
-#     }
-#     return render(request, 'main/profile-delete.html', context)
 
 
 # def delete_profile(request):
