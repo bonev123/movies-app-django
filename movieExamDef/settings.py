92451d1ce3352e779962b9980f9dc4c1e3d35fd6
bonev1234 <bonev123_1996@abv.bg>
2022-05-03 22:25:32 +0300

    extracted Secret Key from settings.py

--- movieExamDef/settings.py
+++ movieExamDef/settings.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--&jth23lruw%7umfch5ax%na!%h%c=s(0c-k$!%)s71(dg@fyo'
-
+SECRET_KEY = config('SECRET_KEY') # Instead of your actual secret key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
